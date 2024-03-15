a94884395205daa38b252b40b1d43fcae3e4c738
Sergiu Mocanu <smocanu@ryerson.ca>
2024-03-15 13:27:10 -0400

    feat: add live view for Game

--- src/pages/2.game/Game.tsx
+++ src/pages/2.game/Game.tsx
@@ -14,9 +14,10 @@ import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle }
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "@/components/ui/carousel"
 import AppTable from "@/components/Table"
-import { Separator } from "@radix-ui/react-select"
 import { Avatar, AvatarFallback } from "@/components/ui/avatar"
-import { AvatarImage } from "@radix-ui/react-avatar"
+import { AvatarImage } from "@/components/ui/avatar"
+import { Separator } from "@/components/ui/separator"
+import { Badge } from "@/components/ui/badge"
 
 const Game = () => {
 
@@ -38,7 +39,7 @@ const Game = () => {
     
     const GameTabs = () => {
         return (
-            <Tabs defaultValue="play-by-play">
+            <Tabs defaultValue="boxscore">
                 <TabsList>
                     <TabsTrigger value="boxscore">Boxscore</TabsTrigger>
                     <TabsTrigger value="summary">Summary</TabsTrigger>
@@ -61,26 +62,26 @@ const Game = () => {
         
         return (
             <Popover>
-            <PopoverTrigger asChild>
-                <Button
-                variant={"outline"}
-                className={cn(
-                    "w-[280px] justify-start text-left font-normal",
-                    !date && "text-muted-foreground"
-                )}
-                >
-                <CalendarIcon className="mr-2 h-4 w-4" />
-                {date ? format(date, "PPP") : <span>Pick a date</span>}
-                </Button>
-            </PopoverTrigger>
-            <PopoverContent className="w-auto p-0">
-                <Calendar
-                mode="single"
-                selected={date}
-                onSelect={setDate}
-                initialFocus
-                />
-            </PopoverContent>
+                <PopoverTrigger asChild>
+                    <Button
+                    variant={"outline"}
+                    className={cn(
+                        "w-[280px] justify-start text-left font-normal",
+                        !date && "text-muted-foreground"
+                    )}
+                    >
+                    <CalendarIcon className="mr-2 h-4 w-4" />
+                    {date ? format(date, "PPP") : <span>Pick a date</span>}
+                    </Button>
+                </PopoverTrigger>
+                <PopoverContent className="w-auto p-0">
+                    <Calendar
+                    mode="single"
+                    selected={date}
+                    onSelect={setDate}
+                    initialFocus
+                    />
+                </PopoverContent>
             </Popover>
         )
     }
@@ -94,24 +95,130 @@ const Game = () => {
             .then(response => response.json())
             .then(json => {
                 setGames(json)
+                console.log(json)
             })
         }, [])
         
         return(
                 <Carousel className="pt-5">
                     <CarouselContent className="-ml-1">
-                    {games?.games?.map((game: any) => 
-                        <CarouselItem className="pl-1 md:basis-1/2 lg:basis-1/5">
-                            <Card className="flex flex-col justify-between">
-                                <CardHeader className="flex-row gap-4 items-center">
-                                    <CardTitle>
-                                        AWAY - HOME
+                    {games?.games?.map((game: any, i:number) => 
+                        <CarouselItem className="md:basis-1/2 lg:basis-1/4">
+                            <Card key={i} className="flex flex-col justify-between">
+                                <CardHeader>
+                                    <CardTitle className="flex justify-between">
+                                        <img src={game.away_team.logo} className="h-20 w-20"/>
+                                        <Separator orientation="vertical"/>
+                                        <img src={game.home_team.logo} className="h-20 w-20"/>
                                     </CardTitle>
+                                    <CardDescription>
+                                        <p className="text-xl">{game.away_team.abbrev} - {game.home_team.abbrev}</p>
+                                        {(game.game_state === "FUT" || game.game_state === "PRE") &&
+                                        <p>{game.away_team.record} : {game.home_team.record}</p>
+                                        }
+                                    </CardDescription>
                                 </CardHeader>
                                 <CardContent>
-                                    {game.label}
+                                {(game.game_state === "FUT" || game.game_state === "PRE") &&
+                                    <div>
+                                        <Badge>{game.start_time}</Badge>
+                                        <h1>Team Leaders</h1>
+                                        <Carousel>
+                                            <CarouselContent>
+                                                {game.team_leaders.map((player: any, i:number) => (
+                                                    <CarouselItem key={i} className="flex pt-2 justify-center">
+                                                        <Card>
+                                                            <CardContent>
+                                                                <Avatar>
+                                                                    <AvatarImage src={player.headshot}/>
+                                                                </Avatar>
+                                                                <p>{player.name.default}: {player.value} {player.category}</p>
+                                                            </CardContent>
+                                                        </Card>
+                                                    </CarouselItem>
+                                                ))}
+                                            </CarouselContent>
+                                            <CarouselPrevious className="left-2"/>
+                                            <CarouselNext className="right-2"/>
+                                        </Carousel>
+                                    </div>}
+                                    {game.game_state === "LIVE" &&
+                                        <div>
+                                            <div>
+                                                <p>Period: {game.period.number} {game.clock.timeRemaining}</p>
+                                                <Badge variant="destructive">{game.game_state}</Badge>
+                                                <p className="text-xl">{game.away_team.score} - {game.home_team.score}</p>
+                                                <p>SOG: {game.away_team.sog} - {game.home_team.sog}</p>
+                                            </div>
+                                            <div>
+                                                {game.goals.length > 0 &&
+                                                <Carousel>
+                                                <CarouselContent>
+                                                    {game.goals.map((goal: any) => (
+                                                        <CarouselItem>
+                                                            <Card>
+                                                                <CardContent>
+                                                                    <div className="flex justify-center">
+                                                                        <Avatar>
+                                                                            <AvatarImage src={goal.mugshot}/>
+                                                                        </Avatar>
+                                                                        <p className="text-sm">({goal.teamAbbrev}) <b>{goal.name.default} ({goal.goalsToDate})</b> P:{goal.period} {goal.timeInPeriod}</p>
+                                                                    </div>
+                                                                    <div>{goal.assists.map((assist: any) => 
+                                                                        <p className="text-xs">{assist.name.default} ({assist.assistsToDate})</p>
+                                                                    )}</div>
+                                                                </CardContent>
+                                                            </Card>
+                                                        </CarouselItem>
+                                                    ))}
+                                                </CarouselContent>
+                                                <CarouselPrevious className="left-1"/>
+                                                <CarouselNext className="right-1"/>
+                                                </Carousel>
+                                                }
+                                            </div>
+                                        </div>}
+                                        {game.game_state === "OFF" && 
+                                        <div>
+                                            <div>
+                                                {game.period.periodType === "OT" && 
+                                                <Badge variant="secondary">FINAL/OT</Badge>}
+                                                {game.period.periodType === "REG" && 
+                                                <Badge variant="secondary">FINAL</Badge>}
+                                                <p className="text-xl">{game.away_team.score} - {game.home_team.score}</p>
+                                                <p>SOG: {game.away_team.sog} - {game.home_team.sog}</p>
+                                            </div>
+                                            <div>
+                                                {game.goals.length > 0 &&
+                                                <Carousel>
+                                                <CarouselContent>
+                                                    {game.goals.map((goal: any) => (
+                                                        <CarouselItem>
+                                                            <Card>
+                                                                <CardContent>
+                                                                    <div className="flex justify-center">
+                                                                        <Avatar>
+                                                                            <AvatarImage src={goal.mugshot}/>
+                                                                        </Avatar>
+                                                                        <p className="text-sm">({goal.teamAbbrev}) <b>{goal.name.default} ({goal.goalsToDate})</b> P:{goal.period} {goal.timeInPeriod}</p>
+                                                                    </div>
+                                                                    <div>{goal.assists.map((assist: any) => 
+                                                                        <p className="text-xs">{assist.name.default} ({assist.assistsToDate})</p>
+                                                                    )}</div>
+                                                                </CardContent>
+                                                            </Card>
+                                                        </CarouselItem>
+                                                    ))}
+                                                </CarouselContent>
+                                                <CarouselPrevious className="left-1"/>
+                                                <CarouselNext className="right-1"/>
+                                                </Carousel>
+                                                }
+                                            </div>
+                                        </div>
+                                        }
                                 </CardContent>
-                                <CardFooter>
+                                <CardFooter className="flex justify-center">
                                     <Button variant={"outline"} id={game.id} onClick={getButtonId}>Details</Button>
                                 </CardFooter>
                             </Card>
