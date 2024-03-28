5530f9ddab8daa5071b236f3d8e57e256d0c7328
Sergiu Mocanu <smocanu@ryerson.ca>
2024-03-28 16:46:03 -0400

    feat: add playerinfo

---
+++ src/pages/4.player/ColumDefs.tsx
@@ -0,0 +1,109 @@
+export const featuredStats = [
+    {
+        header: "GP",
+        accessorKey: "gamesPlayed"
+    },
+    {
+        header: "G",
+        accessorKey: "goals"
+    },
+    {
+        header: "A",
+        accessorKey: "assists"
+    },
+    {
+        header: "P",
+        accessorKey: "points"
+    },
+    {
+        header: "+/-",
+        accessorKey: "plusMinus"
+    },
+    {
+        header: "PIM",
+        accessorKey: "pim"
+    },
+    {
+        header: "GWG",
+        accessorKey: "gameWinningGoals"
+    },
+    {
+        header: "OTG",
+        accessorKey: "otGoals"
+    },
+    {
+        header: "S",
+        accessorKey: "shots"
+    },
+    {
+        header: "S%",
+        accessorKey: "shootingPctg"
+    },
+    {
+        header: "PPG",
+        accessorKey: "powerPlayGoals"
+    },
+    {
+        header: "PPP",
+        accessorKey: "powerPlayPoints"
+    },
+    {
+        header: "SHG",
+        accessorKey: "shorthandedGoals"
+    },
+    {
+        header: "SHP",
+        accessorKey: "shorthandedPoints"
+    },
+]
+
+export const last5Games = [
+    {
+        header: "Date",
+        accessorKey: "date"
+    },
+    {
+        header: "Opponent",
+        accessorKey: "opponent"
+    },
+    {
+        header: "G",
+        accessorKey: "goals"
+    },
+    {
+        header: "A",
+        accessorKey: "assists"
+    },
+    {
+        header: "P",
+        accessorKey: "points"
+    },
+    {
+        header: "+/-",
+        accessorKey: "plusMinus"
+    },
+    {
+        header: "PIM",
+        accessorKey: "pim"
+    },
+    {
+        header: "S",
+        accessorKey: "shots"
+    },
+    {
+        header: "PPG",
+        accessorKey: "ppg"
+    },
+    {
+        header: "SHG",
+        accessorKey: "shg"
+    },
+    {
+        header: "Shifts",
+        accessorKey: "shifts"
+    },
+    {
+        header: "TOI",
+        accessorKey: "toi"
+    },
+]
--- src/pages/4.player/Player.tsx
+++ src/pages/4.player/Player.tsx
@@ -1,37 +1,124 @@
+import AppTable from "@/components/Table"
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"
 import { useEffect, useState } from "react"
-
-const TeamPicker = () => {
-
-  const [teams, setTeams] = useState<any>()
-
-  useEffect(() => {
-    fetch("/api/team")
-    .then(res => res.json())
-    .then(data => setTeams(data))
-  }, [])
-
-  return(
-    <div>
-      <Select>
-        <SelectTrigger>
-          <SelectValue placeholder="Select a Team" />
-        </SelectTrigger>
-        <SelectContent>
-          {teams?.team_names.map((team_name: any, i: number) => (
-            <SelectItem key={i} value={team_name.value}>{team_name.label}</SelectItem>
-          ))}
-        </SelectContent>
-      </Select>
-    </div>
-  )
-}
-
+import { featuredStats, last5Games } from "./ColumDefs"
 
 const Player = () => {
+
+  const [chosenTeam, setChosenTeam] = useState<any>()
+  const [chosenPlayer, setChosenPlayer] = useState<any>()
+
+  const TeamPicker = () => {
+    const [teams, setTeams] = useState<any>()
+    useEffect(() => {
+      fetch("/api/team")
+      .then(res => res.json())
+      .then(data => setTeams(data))
+    }, [])
+  
+    return(
+      <div>
+        <Select value={chosenTeam} onValueChange={setChosenTeam}>
+          <SelectTrigger>
+            <SelectValue placeholder="Select a Team" />
+          </SelectTrigger>
+          <SelectContent>
+            {teams?.team_names.map((team_name: any, i: number) => (
+              <SelectItem key={i} value={team_name.value}>{team_name.label}</SelectItem>
+            ))}
+          </SelectContent>
+        </Select>
+      </div>
+    )
+  }
+
+  const PlayerPicker = () => {
+    const [players, setPlayers] = useState<any>()
+
+    useEffect(() => {
+      fetch(`/api/all_team_players?team=${chosenTeam}`)
+      .then(res => res.json())
+      .then(data => {
+        setPlayers(data)
+      })
+    }, [chosenTeam])
+
+    return(
+      <div>
+        <Select value={chosenPlayer} onValueChange={setChosenPlayer}>
+          <SelectTrigger>
+            <SelectValue placeholder="Select a player"/>
+          </SelectTrigger>
+          <SelectContent>
+            {players?.players.map((player: any) => (
+              <SelectItem value={player.id}>{player.name}</SelectItem>
+            ))}
+          </SelectContent>
+        </Select>
+      </div>
+    )
+
+  }
+
+  const PlayerInfo = () => {
+    const [playerInfo, setPlayerInfo] = useState<any>()
+
+    useEffect(() => {
+      fetch(`/api/player_info?player=${chosenPlayer}`)
+      .then(res => res.json())
+      .then(data => {
+        setPlayerInfo(data)
+        console.log(data)
+      })
+    }, [chosenPlayer])
+
+    return(
+      <div>
+        {typeof(playerInfo) === "undefined" ? (
+          <p>Choose a player from above</p>
+        ) : (
+          <div className="flex">
+          <div className="w-1/3 pt-3">
+          <Card>
+            <CardHeader>
+              <CardTitle>{playerInfo.name}</CardTitle>
+              <CardDescription className="flex justify-center">#{playerInfo.number} | {playerInfo.position} | <img width="35" src={playerInfo.teamLogo}/></CardDescription>
+            </CardHeader>
+            <CardContent>
+            <img src={playerInfo.headshot}/>
+            <p><b>Height: </b>{playerInfo.height}</p>
+            <p><b>Weight: </b>{playerInfo.weight} lbs</p>
+            <p><b>Born:</b> {playerInfo.age}</p>
+            <p><b>Birthplace:</b> {playerInfo.birthplace}</p>
+            <p><b>Draft:</b> {playerInfo.draft}</p>
+            </CardContent>
+          </Card>
+          </div>
+          <div className="w-2/3 pt-3">
+            <h1>{playerInfo.featuredStatsSeason}</h1>
+            <AppTable columns={featuredStats} data={playerInfo.featuredStatsThisSeason}/>
+            <h1>Career</h1>
+            <AppTable columns={featuredStats} data={playerInfo.featuredStatsCareer}/>
+            <h1>Last 5 Games</h1>
+            <AppTable columns={last5Games} data={playerInfo?.last5Games}/>
+          </div>
+          <div>
+          </div>
+          </div>
+        )}
+      </div>
+    )
+
+  }
+
   return (
     <div>
-      <TeamPicker/>
+      <div className="grid grid-cols-2">
+        <TeamPicker/>
+        <PlayerPicker/>
+      </div>
+      <PlayerInfo/>
     </div>
   )
 }
